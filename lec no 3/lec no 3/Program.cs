5535ac29d385f9c91d1cc6b960158e6198916a54
Muhammad Sohail Sabir <sohailsabir6565@gmail.com>
2022-07-31 17:18:30 +0500

    lecture 3

---
+++ lec no 3/lec no 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec_no_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            honda h = new honda();
+            BMW b = new BMW();
+            Toyota t = new Toyota();
+            Suzuki s = new Suzuki();
+
+            h.car_info();
+            h.input1();
+            h.honda_dis();
+
+            b.car_info();
+            b.input2();
+            b.bmw_dis();
+
+            t.car_info();
+            t.input3();
+            t.toyota_dis();
+
+            s.car_info();
+            s.input4();
+            s.suzuki_dis();
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lec no 3/lec no 3/car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec_no_3
+{
+  public  class car
+    {
+      public string name;
+     public string color;
+     public int model;
+
+     public void car_info()
+      {
+          Console.WriteLine(" Enter Name of car:");
+          name = Console.ReadLine();
+          Console.WriteLine(" Enter color of car:");
+          color = Console.ReadLine();
+          Console.WriteLine(" Enter model of car:");
+          model = int.Parse(Console.ReadLine());
+      }
+    }
+  class honda : car
+  {
+      public int mileage;
+      public void input1()
+      {
+          Console.WriteLine(" Enter Mileage of car:");
+          mileage = int.Parse(Console.ReadLine());
+      }
+    public  void honda_dis()
+      {
+          Console.WriteLine("Mileage of honda:" + mileage);
+      }
+  }
+  class BMW : car
+  {
+      public int mileage;
+      public void input2()
+      {
+          Console.WriteLine(" Enter Mileage of car:");
+          mileage = int.Parse(Console.ReadLine());
+      }
+      public void bmw_dis()
+      {
+          Console.WriteLine("Mileage of BMW:" + mileage);
+      }
+  }
+  class Toyota : car
+  {
+      public int mileage;
+      public void input3()
+      {
+          Console.WriteLine(" Enter Mileage of car:");
+          mileage = int.Parse(Console.ReadLine());
+      }
+      public void toyota_dis()
+      {
+          Console.WriteLine("Mileage of Toyota:" + mileage);
+      }
+  }
+  class Suzuki : car
+  {
+      public int mileage;
+      public void input4()
+      {
+          Console.WriteLine(" Enter Mileage of car:");
+          mileage = int.Parse(Console.ReadLine());
+      }
+      public void suzuki_dis()
+      {
+          Console.WriteLine("Mileage of Suzuki:" + mileage);
+      }
+  }
+}
